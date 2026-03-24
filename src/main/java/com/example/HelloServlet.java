86717f0c484ead0828654092044fb7880c160528
varsha-0411 <boppanavarsha411@gmail.com>
2026-03-24 11:52:25 +0530

    Add files via upload

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello from Java Servlet 🚀</h1>");
+    }
+}
